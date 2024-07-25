9049fa767302c8396ffaf5b066cd05518e93a67c
Hein <huseyin.kla1@gmail.com>
2024-07-25 11:59:16 +0300

    control commit

--- src/main/java/com/example/sportclub/SportclubApplication.java
+++ src/main/java/com/example/sportclub/SportclubApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SportclubApplication {
 
+
+	//commit
 	public static void main(String[] args) {
 		SpringApplication.run(SportclubApplication.class, args);
 	}
